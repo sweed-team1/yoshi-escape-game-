20610e12cd84e55e9055b11fceb6e281346439f2
shabzy <106165438+shabzzy@users.noreply.github.com>
2026-08-25 20:23:30 +0100

    Update event listeners to use window.open for links

--- Js/index.js
+++ Js/index.js
@@ -1,22 +1,26 @@
 let play = document.getElementById("play");
 let about = document.getElementById("about");
 let store = document.getElementById("store");
-
 let otherGames = document.getElementById("other");
+
 play.addEventListener("click", function () {
-  window.location.href = "user.html";
-  // let mp5=new Audio('./alex-productions-epic-cinematic-trailer-elite (2).mp3')
-  // mp5.play()
+    window.location.href = "user.html";
 });
+
 about.addEventListener("click", function () {
-  window.location.href = "about.html";
-  // let mp5=new Audio('./alex-productions-epic-cinematic-trailer-elite (2).mp3')
-  // mp5.play()
+    window.location.href = "about.html";
 });
+
 store.addEventListener("click", function () {
-  window.location.href =
-    "https://www.microsoft.com/en-in/store/top-free/apps/pc";
+    window.open(
+        "https://www.microsoft.com/en-in/store/top-free/apps/pc",
+        "_blank"
+    );
 });
+
 otherGames.addEventListener("click", function () {
-  window.location.href = "https://www.microsoft.com/en-in/store/games";
+    window.open(
+        "https://www.microsoft.com/en-in/store/games",
+        "_blank"
+    );
 });
