293c539ad2be4a2789430e224b41340f36f5768d
shabzy <106165438+shabzzy@users.noreply.github.com>
2026-08-26 21:02:51 +0100

    Refactor user input handling and storage logic

--- Js/user.js
+++ Js/user.js
@@ -1,46 +1,60 @@
-let Username
-let getName=localStorage.getItem('userName')
+let Username;
+let getName = localStorage.getItem("userName");
+
 function store() {
     if (getName) {
-        ar=JSON.parse(getName)
+        let ar = JSON.parse(getName);
+        console.log(ar);
     }
-    // start()
 }
 
-document.getElementById('start-btn').addEventListener('click', function start() {
-    console.log("Game started!");
-     myName=document.getElementById('user').value
-    Username=[{name:myName}]
-    // console.log(Username[0].name);
-    let user_regex=/^[a-zA-Z]{6,}$/
-    if (user.value=='') {
-        alert('Username required')
-        return;
-    }
-    if (!user_regex.test(Username[0].name)) {
-        alert('Input should be more than six letters')
-        return;
-    }
-    setTimeout(() => {
-        if (user_regex.test(Username[0].name)) {
-            window.location.href ="./game.html";
-            myName=''
-        }
-    }, 10000);
-    user.value=''
-    localStorage.setItem('userName', JSON.stringify(Username))
-    
-<br>
-let modal = document.getElementById("myModal");
-  modal.style.display = "block";
-  let span = document.getElementsByClassName("close")[0];
-  span.onclick = function () {
-    modal.style.display = "none";
-  };
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      onclick.style.display = "none";
-    }
-  };
+document.getElementById("start-btn").addEventListener("click", function () {
 
-  });
+    console.log("Game started!");
+
+    const user = document.getElementById("user");
+    let myName = user.value.trim();
+
+    Username = [{
+        name: myName
+    }];
+
+    let user_regex = /^[a-zA-Z]{6,}$/;
+
+    if (myName === "") {
+        alert("Username required");
+        return;
+    }
+
+    if (!user_regex.test(myName)) {
+        alert("Username must be at least 6 letters");
+        return;
+    }
+
+    localStorage.setItem(
+        "userName",
+        JSON.stringify(Username)
+    );
+
+    user.value = "";
+
+    let modal = document.getElementById("myModal");
+
+    modal.style.display = "block";
+
+    let span = document.getElementsByClassName("close")[0];
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    };
+
+    setTimeout(() => {
+        window.location.href = "./game.html";
+    }, 1000);
+});
