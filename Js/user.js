8d57675bacf0973a3378ed5323db9c34bff62933
shabzy <106165438+shabzzy@users.noreply.github.com>
2026-08-26 20:59:25 +0100

    Refactor start function for username validation
    Refactored the start function to remove commented code and streamline the username validation process.

--- Js/user.js
+++ Js/user.js
@@ -9,39 +9,38 @@ function store() {
 
 document.getElementById('start-btn').addEventListener('click', function start() {
     console.log("Game started!");
-  });
-//  function start() { 
-//     myName=document.getElementById('user').value
-//     Username=[{name:myName}]
-//     // console.log(Username[0].name);
-//     let user_regex=/^[a-zA-Z]{6,}$/
-//     if (user.value=='') {
-//         alert('Username required')
-//         return;
-//     }
-//     if (!user_regex.test(Username[0].name)) {
-//         alert('Input should be more than six letters')
-//         return;
-//     }
-//     setTimeout(() => {
-//         if (user_regex.test(Username[0].name)) {
-//             window.location.href ="./game.html";
-//             myName=''
-//         }
-//     }, 10000);
-//     user.value=''
-//     localStorage.setItem('userName', JSON.stringify(Username))
+     myName=document.getElementById('user').value
+    Username=[{name:myName}]
+    // console.log(Username[0].name);
+    let user_regex=/^[a-zA-Z]{6,}$/
+    if (user.value=='') {
+        alert('Username required')
+        return;
+    }
+    if (!user_regex.test(Username[0].name)) {
+        alert('Input should be more than six letters')
+        return;
+    }
+    setTimeout(() => {
+        if (user_regex.test(Username[0].name)) {
+            window.location.href ="./game.html";
+            myName=''
+        }
+    }, 10000);
+    user.value=''
+    localStorage.setItem('userName', JSON.stringify(Username))
     
-// <br>
-// let modal = document.getElementById("myModal");
-//   modal.style.display = "block";
-//   let span = document.getElementsByClassName("close")[0];
-//   span.onclick = function () {
-//     modal.style.display = "none";
-//   };
-//   window.onclick = function (event) {
-//     if (event.target == modal) {
-//       onclick.style.display = "none";
-//     }
-//   };
-// }
+<br>
+let modal = document.getElementById("myModal");
+  modal.style.display = "block";
+  let span = document.getElementsByClassName("close")[0];
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      onclick.style.display = "none";
+    }
+  };
+
+  });
