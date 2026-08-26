545515c26047525d3ae21d394a5549bb2b85c3c7
shabzy <106165438+shabzzy@users.noreply.github.com>
2026-08-26 11:39:20 +0100

    Refactor user input handling and storage logic

--- Js/user.js
+++ Js/user.js
@@ -1,53 +1,43 @@
-let Username;
-
-const user = document.getElementById("user");
-
-function start() {
-
-  let myName = user.value.trim();
-
-  const user_regex = /^[a-zA-Z]{6,}$/;
-
-  if (myName === "") {
-    alert("Username required");
-    return;
-  }
-
-  if (!user_regex.test(myName)) {
-    alert("Username should contain at least 6 letters");
-    return;
-  }
-
-  Username = [
-    {
-      name: myName
+let Username
+let getName=localStorage.getItem('userName')
+function store() {
+    if (getName) {
+        ar=JSON.parse(getName)
     }
-  ];
-
-  localStorage.setItem(
-    "userName",
-    JSON.stringify(Username)
-  );
-
-  user.value = "";
-
-  let modal = document.getElementById("myModal");
-
+    // start()
+}
+function start() {
+    myName=document.getElementById('user').value
+    Username=[{name:myName}]
+    // console.log(Username[0].name);
+    let user_regex=/^[a-zA-Z]{6,}$/
+    if (user.value=='') {
+        alert('Username required')
+        return;
+    }
+    if (!user_regex.test(Username[0].name)) {
+        alert('Input should be more than six letters')
+        return;
+    }
+    setTimeout(() => {
+        if (user_regex.test(Username[0].name)) {
+            window.location.href ="./game.html";
+            myName=''
+        }
+    }, 10000);
+    user.value=''
+    localStorage.setItem('userName', JSON.stringify(Username))
+    
+<br>
+let modal = document.getElementById("myModal");
   modal.style.display = "block";
-
   let span = document.getElementsByClassName("close")[0];
-
   span.onclick = function () {
     modal.style.display = "none";
   };
-
   window.onclick = function (event) {
-    if (event.target === modal) {
+    if (event.target == modal) {
       modal.style.display = "none";
     }
   };
-
-  setTimeout(() => {
-    window.location.href = "./game.html";
-  }, 1000);
 }
